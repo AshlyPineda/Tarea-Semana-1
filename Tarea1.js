e980b2388aa2f29d5417f3f1c45390d773d703fd
Ashly <pinedaashly23@gmail.com>
2022-01-29 23:58:56 -0600

    Tarea 1

---
+++ Tarea1.js
@@ -0,0 +1,45 @@
+//Tarea1
+
+let ObjetoEstudiante1 = {
+    id:"78785327",
+    nombre: 'Carlos Pineda',
+    nota: 90,
+    estado: "Aprobado",
+
+}
+
+let ObjetoEstudiante2 = {
+    id:"89785327",
+    nombre: 'Dennis Mora',
+    nota: 70,
+    estado: "Aprobado",
+}
+
+let ObjetoEstudiante3 = {
+    id:"34785327",
+    nombre: 'Marco Paz',
+    nota: 55,
+    estado: "Reprobado",
+}
+
+let ObjetoEstudiante4 = {
+    id:"08755327",
+    nombre: 'Carlos Pineda',
+    nota: 59,
+    estado: "Reprobado",
+
+}
+
+let arrayEst = [
+     {
+         id: '9595803', nombre: 'Pedro', nota: 89, estado: "Aprobado"
+     },
+     ObjetoEstudiante1
+]
+
+arrayEst.push(ObjetoEstudiante3)
+arrayEst.push(50)
+
+let resultadoFiltro = arrayEst.find(estudiante => estudiante.estado === 'Reprobado')
+
+console.log(resultadoFiltro)
